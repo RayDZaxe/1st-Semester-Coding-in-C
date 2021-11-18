12cc7599f68368b03ac0a3591bda8b50cf1679fd
Zaxe <94627517+RayDZaxe@users.noreply.github.com>
2021-11-18 21:16:32 +0530

    Create RelationalOperators.c

---
+++ RelationalOperators.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int a, b;
+a = 3;
+b = 5;
+printf("a>b : %d \n", (a>b));
+printf("a<b : %d \n", (a<b));
+printf("a>=b : %d \n", (a>=b));
+printf("a<=b : %d \n", (a<=b));
+printf("a==b : %d \n", (a==b));
+printf("a!=b : %d \n", (a!=b));
+}
